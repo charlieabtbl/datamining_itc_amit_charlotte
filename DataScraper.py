53ed77b993e3e741c52d39cb593f16fafd7bf006
Amit <Amit21e@gmail.com>
2020-11-08 11:40:05 +0200

    Added docstrings for each method

--- DataScraper.py
+++ DataScraper.py
@@ -24,7 +24,10 @@ class Scrapper:
                       'Job_Description': [],
                       'URL': []}
 
-    def build_pages_result_list(self):
+    def build_pages_result_list(self) -> list:
+        """
+        Building a list of pages urls to scrap later on
+        """
         pages_urls = []
         for page_number in range(1, self._num_pages + 1):
             page_url = self._base_url + str(page_number) + ".htm"
@@ -45,27 +48,53 @@ class Scrapper:
         return self._data
 
     def build_data_dict(self, **kwargs):
-
+        """
+        Fill the _data dictionary with values from each job that was scraped
+        Used at the main() function
+        """
         for key, val in kwargs.items():
             self._data[key].append(val)
 
     def request_page(self, url):
-
+        """
+        Sends an HTTP GET request to a specific URL
+        Being used in main() as the very first step of scraping
+        """
         self.page = requests.get(url, headers=self.headers)
         if self.page.status_code != 200:
             raise ValueError(f"Excpected to get 200\nInstead got: {self.page.status_code}")
 
-    def get_elements(self, html_tag, html_class):
-        self.elements = self.result.find_all(html_tag, class_=fr"{html_class}")
+    def get_elements(self, html_tag, html_class=None):
+        """
+        Acquire specified elements from the 'result' Soup object
+        :param html_tag: str - HTML tag to search for
+        :param html_class: (optional) str - Specifing the HTML tag's class
+        """
+        if html_class:
+            self.elements = self.result.find_all(html_tag, class_=fr"{html_class}")
+        else:
+            self.elements = self.result.find_all(html_tag)
 
-    def init(self, unique_tag_id):
+    def init(self, unique_tag_id: str):
+        """
+        Initializing BeautifulSoup object
+        :param unique_tag_id: string - An HTML tag to search for
+        """
         self.soup = BeautifulSoup(self.page.content, features='html.parser')
         self.result = self.soup.find(id=unique_tag_id)
 
     def build_dataframe(self):
+        """
+        Build pandas DataFrame out of the _data dictionary
+        Being used as the last step of the scarping process
+        """
         self.df = pd.DataFrame(self.data)
 
-    def save_data(self, path):
+    def save_data(self, path: str):
+        """
+        Save results to an external CSV file
+        :param path: str - Path of the new file to save to
+        """
         if ".csv" not in os.path.splitext(path):
             path += ".csv"
         self.df.to_csv(path)
@@ -89,7 +118,9 @@ class Job:
         self.salary = np.nan
 
     def extract_features(self):
-
+        """
+        Extract pre-defined features out of the Job page content
+        """
         # Each job entry has to have COMPANY NAME, TITLE and LOCATION
         self.comp_name = self._job_tag.find("a", class_="css-10l5u4p e1n63ojh0 jobLink").text
         self.title = self._job_tag.find("a", class_="jobInfoItem jobTitle css-13w0lq6 eigr9kq1 jobLink").text
@@ -105,6 +136,9 @@ class Job:
         self.salary = "".join(salary_range[0]) if len(salary_range) else np.nan
 
     def extract_url(self):
+        """
+        Get the URL of the current Job
+        """
         self.url = 'https://www.glassdoor.com' + self._job_tag.find('a')['href']
 
 
