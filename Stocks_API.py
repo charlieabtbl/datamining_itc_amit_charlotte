6f281a18c3eed0789c61f56688fe46379f6bf2ab
Amit <Amit21e@gmail.com>
2020-12-20 00:06:24 +0200

    Added constant to the file

--- Stocks_API.py
+++ Stocks_API.py
@@ -3,18 +3,15 @@ import logging
 import json
 
 logger = logging.getLogger('glassdoor_scraping.log')
+SEARCH = "https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/search"
+PROFILE = "https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/profile/"
 
 
 def extract_info_API(company_name):
     """ The function takes the company_name as a parameter and returns information from the Stock Exchange
     platform regarding the stock price, market capitalization, currency and website of the company """
 
-    with open('config.json', 'r') as config_file:
-        api_params = json.load(config_file)['API']
-
     # Extracting the symbol from the company name
-    api_search = api_params['search']
-
     stock_markets = ['NYSE', 'NASDAQ', 'AMEX', 'EURONEX', 'TSX',
                      'INDEXES', 'ETFs', 'MUTUAL FUNDS', 'FOREX', 'CRYPTO']
 
@@ -22,7 +19,7 @@ def extract_info_API(company_name):
         parameters = {'query': company_name,
                       'limit': '1', 'exchange': market}
 
-        response_symbol = requests.get(api_search, params=parameters)
+        response_symbol = requests.get(SEARCH, params=parameters)
         response_json_symbol = response_symbol.json()
 
         if len(response_json_symbol):
@@ -34,7 +31,7 @@ def extract_info_API(company_name):
                 continue
 
             # Extracting the company_info from the symbol
-            api_profile = api_params['profile'] + str(symbol)
+            api_profile = PROFILE + str(symbol)
 
             response_comp = requests.get(api_profile)
             response_json_comp = response_comp.json()
