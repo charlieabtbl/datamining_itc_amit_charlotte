e91475453c41f684b1163ca4a163326e2ca41983
Amit <Amit21e@gmail.com>
2020-12-20 00:02:36 +0200

    Added function for handling scraping results

--- Results_handler.py
+++ Results_handler.py
@@ -20,3 +20,20 @@ def merge_csvs(filepath):
     for file in files:
         os.remove(file)
 
+
+def create_csv_res_file(company_tab_data, general_data, ratings_tab_data, file_name):
+
+    save_to_csv('comp.csv', company_tab_data, ['Size', 'Founded', 'Type', 'Industry', 'Sector', 'Revenue'])
+
+    save_to_csv('common.csv', general_data, ['Company_Name', 'Job_Title', 'City', 'State',
+                                             'Min_Salary', 'Max_Salary'])
+
+    save_to_csv('ratings.csv', ratings_tab_data, ['Overall', 'Culture & Values', 'Diversity & Inclusion',
+                                                  'Work/Life Balance', 'Senior Management', 'Comp & Benefits',
+                                                  'Career Opportunities'])
+
+    if os.path.exists(file_name):
+        os.remove(file_name)
+
+    merge_csvs(file_name)
+
